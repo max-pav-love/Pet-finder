925bcfc333daaa77569ece4438f010b303c54b50
Maksim Pavlovich <max.pav.love@icloud.com>
2023-07-12 18:13:20 +0300

    Initial Commit

---
+++ Pets-Interview/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Pets-Interview
+//
+//  Created by Maksim Khlestkin on 12.07.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pets-Interview/Pets_InterviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pets_InterviewApp.swift
+//  Pets-Interview
+//
+//  Created by Maksim Khlestkin on 12.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Pets_InterviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
